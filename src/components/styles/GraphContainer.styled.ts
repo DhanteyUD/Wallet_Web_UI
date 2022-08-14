2f7f471bfd1a13b95c9e19df4fa100b57c6e4ba3
DhanteyUD <otseclinton60@gmail.com>
2022-08-14 14:44:20 +0100

    Graph + Total Container

--- src/components/styles/GraphContainer.styled.ts
+++ src/components/styles/GraphContainer.styled.ts
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 export const GraphContainer = styled.div`
   position: relative;
-  width: 57%;
+  width: 63.9%;
   border-radius: 10px;
   height: 33.5%;
   margin-left: 4.5%;
@@ -17,5 +17,19 @@ export const GraphContainer = styled.div`
     font-weight: 600;
     left: 3%;
     top: 8%;
+  }
+
+  @media (max-width: 1000px) {
+    margin-top: 20%;
+    margin-bottom: 20%;
+    width: 91%;
+
+    h1 {
+      position: absolute;
+      font-size: 25px;
+      font-weight: 600;
+      left: 3%;
+      top: -15%;
+    }
   }
 `;
