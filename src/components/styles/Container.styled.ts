80171dddae011de409da95a24715cff1505ae23d
DhanteyUD <otseclinton60@gmail.com>
2022-08-14 14:45:06 +0100

    Container + Dashboard + Sidenav Styles

--- src/components/styles/Container.styled.ts
+++ src/components/styles/Container.styled.ts
@@ -1,8 +1,15 @@
-import styled from "styled-components";
+/** @format */
+
+import styled from 'styled-components';
 
 export const Container = styled.div`
-  display: flex;
+  display: grid;
+  grid: auto-flow / 1fr 6fr;
   flex-direction: row;
   width: 100%;
   height: 100%;
-`
+
+  @media (max-width: 1000px) {
+    display: flex;
+  }
+`;
