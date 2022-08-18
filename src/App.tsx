018cf8e33d79fa0a74f0577737496c0e01bc9caf
DhanteyUD <otseclinton60@gmail.com>
2022-08-18 21:08:19 +0100

    App update

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,18 @@
 /** @format */
 
 import './App.css';
-import BarChart from './components/Activities/BarChart';
-import Earnings from './components/Earnings';
 import { Container } from './components/styles/Container.styled';
 import { Sidenav } from './components/styles/Sidenav.styled';
 import { Dashboard } from './components/styles/Dashboard.styled';
+import Sidebar from './components/SideBarComponent/SideBar';
+import Header from './components/HeaderComponent/Header';
 import Totals from './components/Totals';
 import SalesAnalytics from './components/SalesAnalytics';
 import SellingProducts from './components/SellingProducts';
-import Sidebar from './components/SideBarComponent/SideBar';
-import Header from './components/HeaderComponent/Header';
+import MonthlyEarnings from './components/MonthlyEarnings';
+import CustomerActivities from './components/CustomerActivities';
 
 function App() {
-  const barchartData = [
-    {
-      title: 'Mon',
-      value: 50,
-    },
-    {
-      title: 'Tue',
-      value: 80,
-    },
-    {
-      title: 'Wed',
-      value: 20,
-    },
-    {
-      title: 'Thu',
-      value: 30,
-    },
-    {
-      title: 'Fri',
-      value: 75,
-    },
-    {
-      title: 'Sat',
-      value: 55,
-    },
-    {
-      title: 'Sun',
-      value: 35,
-    },
-  ];
 
   return (
     <>
@@ -55,14 +25,9 @@ function App() {
           <Totals />
           <SalesAnalytics />
           <SellingProducts />
-
           <div className="App">
-            <header className="App-header">
-              <Earnings />
-            </header>
-            <div className="customer-activities">
-              <BarChart data={barchartData} />
-            </div>
+            <MonthlyEarnings />
+            <CustomerActivities />
           </div>
         </Dashboard>
       </Container>
